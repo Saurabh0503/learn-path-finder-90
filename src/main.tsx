088fba11c3979c399d8858980cdaa1afa5492a9e
Saurabh0503 <79012675+Saurabh0503@users.noreply.github.com>
2025-09-17 01:32:11 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { VideoCacheProvider } from "@/context/VideoCacheContext";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <VideoCacheProvider>
+    <App />
+  </VideoCacheProvider>
+);
